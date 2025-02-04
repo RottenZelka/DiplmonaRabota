0b31143e02d298f971adb99c8c6a0b1920bbba95
RottenZelka <m.staykov06@gmail.com>
2025-02-04 22:10:00 +0200

    test 2

--- yii2-backend/config/db.php
+++ yii2-backend/config/db.php
@@ -1,18 +1,9 @@
 <?php
 
-$jawsdb_url = 'mysql://qvf0m4o49xz821bb:fspnankuwjg142ym@w1kr9ijlozl9l79i.chr7pe7iynqr.eu-west-1.rds.amazonaws.com:3306/hp5uwy9l7y5jhy59';
-
-$url = parse_url($jawsdb_url);
-
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => sprintf(
-        'mysql:host=%s;port=%d;dbname=%s',
-        $url['host'],
-        isset($url['port']) ? $url['port'] : 3306,
-        ltrim($url['path'], '/')
-    ),
-    'username' => $url['user'],
-    'password' => isset($url['pass']) ? $url['pass'] : '',
+    'dsn' => 'mysql:host=127.0.0.1;port=3307;dbname=yii2db', // Use 127.0.0.1 instead of `db`
+    'username' => 'yii2user',
+    'password' => 'yii2password',
     'charset' => 'utf8',
 ];
